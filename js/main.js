09e0dbc957286ad6ccdd8c08e999e93cfaff7ff4
gm-dev <46127390+georgm-dev@users.noreply.github.com>
2022-07-24 15:23:34 +0500

    improvements

--- js/main.js
+++ js/main.js
@@ -291,29 +291,28 @@
 
 
 $(function() {
-
-  $(".progress").each(function() {
-
-    var value = $(this).attr('data-value');
-    var left = $(this).find('.progress-left .progress-bar');
-    var right = $(this).find('.progress-right .progress-bar');
-
-    if (value > 0) {
-      if (value <= 50) {
-        right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
-      } else {
-        right.css('transform', 'rotate(180deg)')
-        left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
-      }
-    }
-
-  })
-
-  function percentageToDegrees(percentage) {
-
-    return percentage / 100 * 360
-
+  const chartWrapper = document.querySelector(".chart-wrapper");
+  const scrollDown = document.querySelector(".scroll-down");
+  
+  function isElementInViewport(el) {
+	var rect = el.getBoundingClientRect();
+	return (
+	  rect.top >= 0 &&
+	  rect.left >= 0 &&
+	  rect.bottom <=
+		(window.innerHeight || document.documentElement.clientHeight) &&
+	  rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
   }
-
+  
+  window.addEventListener("scroll", scrollHandler);
+  
+  function scrollHandler() {
+	window.pageYOffset > 0
+	  ? scrollDown.classList.add("is-hidden")
+	  : scrollDown.classList.remove("is-hidden");
+	if (isElementInViewport(chartWrapper)) chartWrapper.classList.add("in-view");
+  }
+  
 });
 
