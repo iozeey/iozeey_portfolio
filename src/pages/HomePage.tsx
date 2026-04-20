4a83e291a3eae4effdd89fc51192bca499eec6e2
Zeeshan <zeeshan.za.ahmad@gmail.com>
2026-04-20 21:19:59 +0100

    chore: Adjust spacing in HomePage component for improved layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -18,6 +18,7 @@ const HomePage: React.FC = () => {
         <SkillsSection />
         <ServicesSection />
         <ProjectsSection />
+        
         {/* <CounterSection /> */}
         <HireMeSection />
       </main>
